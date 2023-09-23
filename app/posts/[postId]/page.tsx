f4d7ff2a414139ca26fc63cbc7ace93ab16a5377
jorgegoco <jorgegoco70@gmail.com>
2023-09-23 18:41:25 +0200

    Create dynamic pages for posts

---
+++ app/posts/[postId]/page.tsx
@@ -0,0 +1,32 @@
+import { getSortedPostsData } from '@/lib/posts'
+import { notFound } from 'next/navigation'
+
+export function generateMetadata({ params }: { params: { postId: string } }) {
+
+  const posts = getSortedPostsData()
+  const { postId } = params
+
+  const post = posts.find(post => post.id === postId)
+
+  if (!post) {
+      return {
+          title: 'Post Not Found'
+      }
+  }
+
+  return {
+      title: post.title,
+  }
+}
+
+export default async function Post({ params }: { params: { postId: string } }) {
+
+  const posts = getSortedPostsData()
+  const { postId } = params
+
+  if (!posts.find(post => post.id === postId)) notFound()
+
+  return (
+    <div>page</div>
+  )
+}

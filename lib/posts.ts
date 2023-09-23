da59473863f0c049ae1cec4475447d2493b5f890
jorgegoco <jorgegoco70@gmail.com>
2023-09-23 21:07:38 +0200

    Get the posts content for the dynamic pages

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,6 +1,8 @@
 import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
+import { remark } from 'remark'
+import html from 'remark-html'
 
 const postsDirectory = path.join(process.cwd(), 'blogposts')
 
@@ -30,3 +32,28 @@ export function getSortedPostsData() {
     // Sort posts by date
     return allPostsData.sort((a, b) => a.date < b.date ? 1 : -1);
 }
+
+export async function getPostData(id: string) {
+  const fullPath = path.join(postsDirectory, `${id}.md`);
+  const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+  // Use gray-matter to parse the post metadata section
+  const matterResult = matter(fileContents);
+
+  const processedContent = await remark()
+      .use(html)
+      .process(matterResult.content) as unknown as { toString(): string }
+
+  const contentHtml = processedContent.toString()
+
+  const blogPostWithHTML: BlogPost & { contentHtml: string } = {
+      id,
+      title: matterResult.data.title,
+      date: matterResult.data.date,
+      contentHtml,
+  }
+
+  // Combine the data with the id
+  return blogPostWithHTML
+}
+

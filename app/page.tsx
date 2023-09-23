dfdfc105fa8d3deff6c6920871180eb633f1e918
jorgegoco <jorgegoco70@gmail.com>
2023-09-23 13:07:30 +0200

    Add main welcome presentation

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
-    <main>
-      
+    <main className="px-6 mx-auto">
+      <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
+        Hello and Welcome 👋&nbsp;
+        <span className="whitespace-nowrap">
+          I&apos;m <span className="font-bold">Jorge</span>.
+        </span>
+      </p>
     </main>
   )
 }
